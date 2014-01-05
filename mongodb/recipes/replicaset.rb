c6e1cb663dcaf8e26014814133fc0d0a069b0289
Doug Bryant <doug@netinlet.com>
2014-01-05 14:46:25 -0800

    Checking if opsworks replicaset make it work

--- mongodb/recipes/replicaset.rb
+++ mongodb/recipes/replicaset.rb
@@ -20,17 +20,26 @@
 include_recipe "mongodb"
 include_recipe "mongodb::mongo_gem"
 
-Chef::Log.info "Configuring replicaset with STD REPLICASET"
+::Chef::Recipe.send(:include, MongoDB::OpsWorksHelper)
+
+Chef::Log.info "Configuring replicaset with OPSWORKS REPLICASET"
 
 # if we are configuring a shard as a replicaset we do nothing in this recipe
 if !node.recipe?("mongodb::shard")
-  mongodb_instance node['mongodb']['instance_name'] do
-    mongodb_type "mongod"
-    port         node['mongodb']['port']
-    logpath      node['mongodb']['logpath']
-    dbpath       node['mongodb']['dbpath']
-    replicaset   node
-    enable_rest  node['mongodb']['enable_rest']
-    smallfiles   node['mongodb']['smallfiles']
-  end
+
+  # assuming for the moment only one layer for the replicaset instances
+  replicaset_layer_slug_name = node['opsworks']['instance']['layers'].first
+  replicaset_layer_instances = node['opsworks']['layers'][replicaset_layer_slug_name]['instances']
+
+  Chef::ResourceDefinitionList::MongoDB.configure_replicaset(node, replicaset_layer_slug_name, replicaset_members(node, replicaset_layer_instances))
 end
+
+  # mongodb_instance node['mongodb']['instance_name'] do
+  #   mongodb_type "mongod"
+  #   port         node['mongodb']['port']
+  #   logpath      node['mongodb']['logpath']
+  #   dbpath       node['mongodb']['dbpath']
+  #   replicaset   node
+  #   enable_rest  node['mongodb']['enable_rest']
+  #   smallfiles   node['mongodb']['smallfiles']
+  # end
